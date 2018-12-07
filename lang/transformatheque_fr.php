dcb6014b225562da5ea2d6400040087a66230bc4
abelass <rainer@websimple.be>
2018-12-07 16:44:33 +0100

    ça avance

--- lang/transformatheque_fr.php
+++ lang/transformatheque_fr.php
@@ -10,9 +10,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'contactez_nous' => 'Contactez nous',
 
+	// I
+	'inscription' => 'Inscription',
+
+	// P
+	'places' => 'places',
+
 	//S
 	'savoir_plus' => 'En savoir plus',
 
 	// T
 	'transformatheque_titre' => 'Squelette transformathèque',
+	'voir_tous_projets' => 'Voir tous les projets',
 );
