1931f741643595b84dc79c853c2b4e95e53860f2
Rainer Müller <rainer@websimple.be>
2018-12-12 16:07:59 +0100

    nécessite iextras

--- lang/transformatheque_fr.php
+++ lang/transformatheque_fr.php
@@ -13,19 +13,33 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// C
 	'contactez_nous' => 'Contactez nous',
 
+	// D
+	'detail' => 'Détail @type@',
+
+	// E
+	'equipement' => 'Equipment',
+
+
 	// I
 	'inscription' => 'Inscription',
 
 	// N
 	'nouveautes' => 'Nouveautés',
 
+	// O
+	'offrez_cheque_cadeau' => 'Offrez un chèque cadeau',
+
 	// P
 	'places' => 'places',
 	'projets_cours' => 'Projects en cours',
 	'plus_info' => 'Plus d\'info',
 
+	// R
+	'retour' => 'Retour aux @type@s',
+
 	//S
 	'savoir_plus' => 'En savoir plus',
+	'seance_nr' =>'Cours @compteur@',
 
 	// T
 	'texte_newslette' => 'Sign up to our newsletter to receive various informations that can be of use for you and your family in times of urgent need.',
