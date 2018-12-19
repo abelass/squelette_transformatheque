e6d56fb9c4f3acbee8fd0b4ceedc61862269426c
Rainer Müller <rainer@websimple.be>
2018-12-19 10:53:49 +0100

    oups

--- lang/paquet-transformatheque_fr.php
+++ lang/paquet-transformatheque_fr.php
@@ -2,13 +2,13 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')) {
-	return;
+  return;
 }
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// T
-	'transformatheque_description' => '',
-	'transformatheque_nom' => 'Squelette transformathèque',
-	'transformatheque_slogan' => '',
+  // T
+  'transformatheque_description' => '',
+  'transformatheque_nom' => 'Squelette transformathèque',
+  'transformatheque_slogan' => '',
 );
